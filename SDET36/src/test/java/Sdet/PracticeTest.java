9a4ec1d5a9726e2e02f17d43f1e1234f4367d483
Keshavroy12345 <Keshu1123@gmail.com>
2022-08-03 14:54:35 +0530

    done

---
+++ SDET36/src/test/java/Sdet/PracticeTest.java
@@ -0,0 +1,12 @@
+package Sdet;
+
+import org.testng.annotations.Test;
+
+public class PracticeTest {
+	@Test
+	public void display() {
+		System.out.println("Test1");
+		System.out.println("Test2");
+	}
+
+}
